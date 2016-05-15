93453a017c9ee9b8a6f14ad5cf3ebce4504f7d1e
Bohdan Simianyk <someonetcool@gmail.com>
2016-05-15 16:39:16 +0300

    Created solution with architecture. Added some packages.

---
+++ RecomenderLibrary/RL.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RL.Web.MvcApplication" Language="C#" %>
---
+++ RecomenderLibrary/RL.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RL.Web.Startup))]
+namespace RL.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
